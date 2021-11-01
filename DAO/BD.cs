3a410996c0a92744c65d07e59ed47fba1a981a85
Lucas Camacho <lucas.camacho@soulasalle.com.br>
2021-11-01 14:32:56 -0300

    2.0

---
+++ DAO/BD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+
+namespace DAO
+{
+    public static class BD
+    {
+        //Data Source=C:\Users\lscamacho\Documents\Faculdade\CTPI\AppAula20212-master\BD.sdf;Password=1998
+        public static void AbrirConexao() 
+        {
+            
+        }
+
+        public static void FecharConexao() 
+        {
+
+        }
+        public static DataTableReader ExecutarSelect(String _SQL)
+        {
+
+        }
+        public static int ExecutarIDU(String _SQL)
+        {
+
+        }
+        public static int ExecutarIDU(String _SQL, List<SqlCeParameter> parameters)
+        {
+
+        }
+        public static Object ExecutarInsertComRetornoDeValor(String _Sql, List<SqlCeParameter> parameters)
+        {
+
+        }
+    }
+}
